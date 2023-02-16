d6adc4768f7273f35deef2cff7677ab149c10753
Kyle Patterson <kuweiyo@gmail.com>
2023-02-16 01:19:39 -0500

    Updated homepage to use react containers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import {Container, Row, Col} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -6,7 +7,21 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                        <h5>this will be site navigation!</h5></Col>
+                        <h5>Links</h5></Col>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
                     <Col xs='6' sm='3' className='text-center'>
                     </Col><h5>Social</h5>
                         <a
