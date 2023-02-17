2f1d81e530969bd08cacd0a677a58ba8e258fb7e
Kyle Patterson <kuweiyo@gmail.com>
2023-02-17 01:07:09 -0500

    Added comments to Campsite Details page

---
+++ src/pages/CampsiteDetailPage.js
@@ -0,0 +1,22 @@
+import { Container, Row } from "reactstrap";
+import { useParams } from "react-router-dom";
+import { selectCampsiteById } from "../features/campsites/campsitesSlice";
+import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import CommentsList from "../features/comments/CommentsList";
+
+
+const Campsite = () => {
+  const { campsiteId } = useParams();
+  const campsite = selectCampsiteById(campsiteId);
+
+  return (
+    <Container>
+      <Row>
+        <CampsiteDetail campsite={campsite} />
+        <CommentsList campsiteId={campsiteId} />
+      </Row>
+    </Container>
+  );
+};
+
+export default Campsite;
