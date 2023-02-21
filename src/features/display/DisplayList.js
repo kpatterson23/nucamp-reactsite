485b15ca3b9bcc43873284ce6ff75d39dbceeb8d
Kyle Patterson <kuweiyo@gmail.com>
2023-02-20 22:25:18 -0500

    Added animation styling to campsite cards

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,19 +1,26 @@
 import { Col, Row } from "reactstrap";
-import DisplayCard from "./DisplayCard";
+// import DisplayCard from "./DisplayCard";
+import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectedFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectedFeaturedPartner } from "../partners/partnersSlice";
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectedFeaturedPromotion(), selectedFeaturedPartner()];
+  const items = [
+    selectFeaturedCampsite(),
+    selectedFeaturedPromotion(),
+    selectedFeaturedPartner(),
+  ];
 
   return (
     <Row>
       {items.map((item, idx) => {
         return (
-          <Col md className="m-1" key={idx}>
-            <DisplayCard item={item} />
-          </Col>
+          item && (
+            <Col md className="m-1" key={idx}>
+              <AnimatedDisplayCard item={item} />
+            </Col>
+          )
         );
       })}
     </Row>
