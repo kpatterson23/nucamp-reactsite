21970236a6070f516b0f0e0975ec01665a3b8930
Kyle Patterson <kuweiyo@gmail.com>
2023-02-20 22:09:45 -0500

    Added page sponsors

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -2,9 +2,10 @@ import { Col, Row } from "reactstrap";
 import DisplayCard from "./DisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectedFeaturedPromotion } from "../promotions/promotionsSlice";
+import { selectedFeaturedPartner } from "../partners/partnersSlice";
 
 const DisplayList = () => {
-  const items = [selectFeaturedCampsite(), selectedFeaturedPromotion()];
+  const items = [selectFeaturedCampsite(), selectedFeaturedPromotion(), selectedFeaturedPartner()];
 
   return (
     <Row>
