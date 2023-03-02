a79ef862c922ce4a04316242444e9080fdab0501
Kyle Patterson <kuweiyo@gmail.com>
2023-03-01 22:35:45 -0500

    Redux for All

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -1,5 +1,19 @@
 import { PROMOTIONS } from "../../app/shared/PROMOTIONS";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const selectedFeaturedPromotion = () => {
-  return PROMOTIONS.find((promotion) => promotion.featured);
+const initialState = {
+  promotionsArray: PROMOTIONS,
+};
+
+const promotionsSlice = createSlice({
+  name: "promotions",
+  initialState,
+});
+
+export const promotionsReducer = promotionsSlice.reducer;
+
+export const selectedFeaturedPromotion = (state) => {
+  return state.promotions.promotionsArray.find(
+    (promotion) => promotion.featured
+  );
 };
